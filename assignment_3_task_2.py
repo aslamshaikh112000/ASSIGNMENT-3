235fcf87568f1aef1b732b6b63021d1207c64940
Shaikh Aslam <aslamshaikh112000@gmail.com>
2025-11-15 23:54:17 +0530

    ASSIGNMENT 3: Module 4: Functions & Modules in Python
    Task 1: Calculate Factorial Using a Function 
    Task 2: Using the Math Module for Calculations
    solve done

---
+++ assignment_3_task_2.py
@@ -0,0 +1,8 @@
+import math
+num = int(input("Enter a number: "))
+output = math.sqrt(num)
+print(f"Square root: {output}")
+output = math.log(num)
+print(f"Logarithmic root: {output}")
+output = math.sin(num)
+print(f"Sine : {output}")
